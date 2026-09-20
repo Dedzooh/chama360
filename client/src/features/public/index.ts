bcc0347f61d154958e889aa4de99f325bb73dfb8
Dedzooh <kimdedan95@gmail.com>
2026-09-20 23:18:04 +0300

    Track public frontend feature module

---
+++ client/src/features/public/index.ts
@@ -0,0 +1,5 @@
+export { DownloadApp } from './DownloadApp.tsx';
+export { Legal } from './Legal.tsx';
+export { JoinInvite } from './JoinInvite.tsx';
+export { JoinOrganizationInvite } from './JoinOrganizationInvite.tsx';
+
